d696dd2a8225f695a1bd8ae4eafa4c14014bdf6e
Ram Parashar <ram.parashar@8x8.com>
2012-12-12 08:16:00 +0530

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MmtoolWeb::Application.config.secret_token = 'e35e515ba92f76ddfd23bc3a460cbc285cb40a38af59ab6c1c4b5124978a4e5c0664c30e61bd00ff2685eb993967da0645a7e6ff64eee352076ec6360c4a5020'
